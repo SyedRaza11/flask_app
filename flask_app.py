1184324425922462825b4040b7c457d3484854db
SyedRaza11 <91846661+SyedRaza11@users.noreply.github.com>
2023-02-04 14:28:19 +0500

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,14 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route(rule = "/", methods = ['GET'])
+
+def main():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1" , port="8080", debug =True)
+
